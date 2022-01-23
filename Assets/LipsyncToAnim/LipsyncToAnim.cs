6c205cdb9d63d38ba8259418681b9924edba1ad1
TonyGamer <tomblackaz@gmail.com>
2022-01-23 12:24:06 -0700

    Added automatic path finding, minimum value, gain, and optimized keyframe placement

--- Assets/LipsyncToAnim/LipsyncToAnim.cs
+++ Assets/LipsyncToAnim/LipsyncToAnim.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
-#if UNITY_EDITOR
+//#if UNITY_EDITOR
 [ExecuteInEditMode]
 public class LipsyncToAnim : MonoBehaviour
 {
     [Header("Required Assets")]
     [Tooltip("The lipsync file to use")]
-    public OVRLipSyncSequence ovrLipSyncSequence;
+    public AudioClip audioClip;
     [Tooltip("The animation clip to modify (contents will be preserved)")]
     public AnimationClip animationClip;
 
+    [Header("Oculus Tweaks")]
+    [Tooltip("Whether to run the Oculus Lipsync locally or not")]
+    public bool offline = false;
+
     [Header("Animation Tweaks")]
-    [Tooltip("Mouth Oject Path")]
-    public string mouthPath = "";
+    [Tooltip("The mesh to be animated")]
+    public SkinnedMeshRenderer mouthObject;
+    [Tooltip("How much to shift the animation by (in milliseconds)")]
+    public float timeShift = 0;
 
     [Header("Viseme Names")]
     [Tooltip("This will be placed before every viseme name")]
@@ -24,10 +31,16 @@ public class LipsyncToAnim : MonoBehaviour
     public string[] visemeNames = Enum.GetNames(typeof(OVRLipSync.Viseme));
 
     [Header("Viseme Tweaks")]
-    [Tooltip("Min Value to have")]
-    public float minValue = 0;
-    [Tooltip("How Much To Multiply Blendshapes By")]
+    [Tooltip("Minimum value to set a viseme to")]
+    [Range(0, 100)]
+    public float minValue = 15;
+    [Tooltip("How much to multiply blendshapes by")]
+    [Range(0, 100)]
     public float gain = 100;
+    [Tooltip("How aggresively to remove keyframes (Recommended values: 1 - 10000)")]
+    public float decimateValue = 3000;
+    [Tooltip("Optimize keyframes (significantly improves performance in game)")]
+    public bool optimize = true;
 
     [Space]
     [Tooltip("Click here to convert the lipsync to an animation")]
@@ -45,15 +58,35 @@ public class LipsyncToAnim : MonoBehaviour
     [ContextMenu("Convert Lipsync")]
     void Convert()
     {
+        int keyframes = 0;
+
         Debug.Log("Beginning Conversion...");
 
-        float fps = ovrLipSyncSequence.entries.Count/ovrLipSyncSequence.length;
+        string path = GetGameObjectPath(mouthObject.transform) ?? "";
 
-        string path = mouthPath ?? "";
+        MatchCollection mc = Regex.Matches(path, @"\b(?<=" + this.gameObject.name + @"\/)[\s\S]*");
+
+        if (mc.Count == 0)
+        {
+            Debug.LogError("Error: No suitable path to selected viseme object");
+        } else
+        {
+            path = mc[0].Value;
+            Debug.Log("Path: " + path);
+        }
+
+        OVRLipSyncSequence ovrLipSyncSequence = OVRLipSyncSequence.CreateSequenceFromAudioClip(audioClip, offline);
+
+        float fps = ovrLipSyncSequence.entries.Count/ovrLipSyncSequence.length;
+        float dt = 1 / fps;
+
         float viseme = 0;
 
         EditorCurveBinding[] editorCurves = new EditorCurveBinding[15];
         AnimationCurve[] animationCurves = new AnimationCurve[15];
+
+        float[] prevSlopes = new float[15];
+        float[] prevVisemes = new float[15];
 
         for(int i = 0; i < 15; i++)
         {
@@ -66,12 +99,17 @@ public class LipsyncToAnim : MonoBehaviour
             animationCurves[i] = new AnimationCurve();
         }
 
+        float prevTime = 0;
+
         foreach (OVRLipSync.Frame frame in ovrLipSyncSequence.entries)
         {
-            float time = frame.frameNumber/fps;
+            float time = frame.frameNumber * dt;
 
             for (int i = 0; i < 15; i++)
             {
+
+                AnimationCurve curve = animationCurves[i];
+
                 viseme = Mathf.Clamp(frame.Visemes[i] * gain, 0, 100);
 
                 if (viseme < minValue)
@@ -79,16 +117,51 @@ public class LipsyncToAnim : MonoBehaviour
                     viseme = 0;
                 }
 
-                AnimationCurve curve = animationCurves[i];
-                curve.AddKey(time, viseme);
+                if (optimize)
+                {
+                    if (curve.length > 2)
+                    {
+                        float slope = viseme - prevVisemes[i];
+                        slope *= fps; //fps = 1/dt. This is an optimization. Think of this as slope /= dt instead.
+
+                        if (Mathf.Sign(slope) == Mathf.Sign(prevSlopes[i])) {
+                            float curvature = slope - prevSlopes[i];
+                            curvature *= fps;
+
+                            if (curvature < decimateValue)
+                            {
+                                prevVisemes[i] = viseme;
+                                continue;
+                            }
+                        }
+
+                        prevSlopes[i] = slope;
+                    }
+                }
+
+                keyframes++;
+                curve.AddKey(prevTime + (0.001f*timeShift), prevVisemes[i]);
+
+                prevVisemes[i] = viseme;
             }
+
+            prevTime = time;
         }
 
         for(int i = 0; i < 15; i++)
         {
-            AnimationUtility.SetEditorCurve(animationClip, editorCurves[i], animationCurves[i]);
+            AnimationCurve curve = animationCurves[i];
+
+            for(int j = 0; j < curve.length; j++)
+            {
+                AnimationUtility.SetKeyLeftTangentMode(curve, j, AnimationUtility.TangentMode.Linear);
+                AnimationUtility.SetKeyRightTangentMode(curve, j, AnimationUtility.TangentMode.Linear);
+            }
+
+            AnimationUtility.SetEditorCurve(animationClip, editorCurves[i], curve);
         }
 
+        Debug.Log(keyframes + "keyframes were generated");
         Debug.Log("Conversion complete!");
     }
 
@@ -103,4 +176,4 @@ public class LipsyncToAnim : MonoBehaviour
         return path;
     }
 }
-#endif
+//#endif
--- Assets/LipsyncToAnim/README.md
+++ Assets/LipsyncToAnim/README.md
@@ -6,13 +6,32 @@ This is a simple script which automatically converts Oculus Lipsync data into a 
 - Use the [lipsync precompute feature](https://developer.oculus.com/documentation/unity/audio-ovrlipsync-precomputed-unity/) to generate the required OVRLipSyncSequence file
 
 ## Converting the lipsync data
-1. Drag the Converter prefab into a scene
-2. Select the prefab in the scene
-3. Drag the lipsync sequence into "Ovr Lip Sync Sequence"
+1. Click on the avatar you wish to animate
+2. Click "Add Component" and add the component "Lipsync To Anim"
+3. Drag the audio into "Audio Clip"
 4. Drag the animation clip you wish to add lipsync to into "Animation Clip" (create a new animation clip if you don't want to modify one)
-5. Setup the viseme names. A prefix option is provided for convenience, leave it blank if your visemes do not use a prefix
-6. Click the convert button
-7. If all went well, you should see the messages "Beginning Conversion..." and "Conversion complete!" back to back with no errors inbetween
+5. Drag the mesh you wish to animate to the "Mouth Object" section
+6. Setup the viseme names. A prefix option is provided for convenience, leave it blank if your visemes do not use a prefix
+7. Change any [settings](#settings) you wish
+8. Click the convert button
+9. If all went well, you should see the messages "Beginning Conversion..." and "Conversion complete!" appear
+
+## Settings
+- Oculus Tweaks
+	- Offline Mode: This toggles the offline mode for the Oculus Lipsync SDK
+- Animation Tweaks
+	- Time Shift: Shift the timing of the animation. This value is in milliseconds
+- Viseme Tweaks:
+	- Min Value: The value below which all viseme keyframes are set to zero (0-100 range)
+	- Gain: The multiplier applied to the viseme values from the SDK (0-100 range)
+	- Decimate Value: Determines how aggresively to remove keyframes. There is no hard limit, but its affects are theoretically present from values 0-369,800. Recommended value are below 20,000
+	- Optimize: Whether or not to remove keyframes which are deemed unnecessary. How many are removed is dependant on the decimation value. Using with with min value and decimate value can reduce the number of keyframes by up to 90% !IT IS HIGHLY RECOMMENDED TO KEEP THIS ENABLED!
+- Convert:
+	- This is not a setting, but rather a button. Clicking this will show no visible feedback, but will run the script with the given settings. You can tell it worked through the message "Conversion Complete" appearing in the console.
 
 ## Issues
 If you have any issues, report the issue on [Github](https://github.com/TonyGamer/LipsyncHelper/issues)
+
+## Credits
+- Mouth Mesh (In the test scene): https://booth.pm/ja/items/1694770
+- Test Audio: The FitnessGram™ Pacer Test
